e0d4842e88da5de9eadb9062244d46e574b734c8
NatashaZettler <natasha.zettler@nubank.com.br>
2025-01-27 10:59:00 -0300

    organize

--- src/pokemons/core.clj
+++ src/pokemons/core.clj
@@ -1,6 +1,6 @@
 (ns pokemons.core
   (:require [pokemons.controller-per-functionality :as p.controller]
-  [pokemons.view :as p.view]))
+            [pokemons.view :as p.view]))
 (p.controller/get-names p.controller/get-results)
 (defn handle-choice [choice]
   (case choice
@@ -25,7 +25,7 @@
 
             )
     "7" (do (println "Você escolheu Opção 7")
-
+            ;Buscar movimentos que comecem com uma palavra especifica
             )
     "8" (do (println "Você escolheu Opção 8")
             (p.controller/search-moves-by-word p.controller/parse-moves "MOVE - ")
@@ -35,12 +35,12 @@
             )
     "10" (do (println "Você escolheu Opção 10")
              (p.controller/get-url-by-name "squirtle")
-            )
+             )
     "11" (do (println "Você escolheu Opção 11")
              (p.controller/sort-names-desc p.controller/get-results)
-            )
+             )
     "12" (do (println "Encerrando o sistema...")
-            (System/exit 0))
+             (System/exit 0))
     (println "Opção inválida, tente novamente.")))
 
 (defn menu-loop []
@@ -51,10 +51,3 @@
       (recur))))
 
 ;(menu-loop)
-
-
-
-
-
-;Comparar se as habilidades de dois pokemons são iguais
-;Buscar movimentos que comecem com uma palavra especifica
